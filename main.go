590cfa6e7c02bfd27fa65a483ef65a72893661eb
ikayrat <ik.kairat@gmail.com>
2023-02-13 17:19:54 +0600

    removed all depencies

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/spf13/viper"
 
 	_ "github.com/lib/pq"
 )
@@ -27,20 +26,16 @@ var numericKeyboard = tgbot.NewReplyKeyboard(
 )
 
 func main() {
-	ctx := context.Background()
 
-	conf, err := LoadConfig(".")
-	if err != nil {
-		log.Panic("cannot load env", err)
-	}
-	log.Println(conf)
+	TOKEN := os.Getenv("TELEGRAM_TOKEN")
+	DBSOURCE := os.Getenv("DBSOURCE")
 
-	db := DBconnection(conf.DatabaseSource)
+	db := DBconnection(DBSOURCE)
 	if err := db.Ping(); err != nil {
 		log.Panic(err)
 	}
 
-	bot, err := tgbot.NewBotAPI(conf.Token)
+	bot, err := tgbot.NewBotAPI(TOKEN)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -64,8 +59,7 @@ func main() {
 
 		// Check if user is already registered
 		var days int
-		// err := db.QueryRow("SELECT days FROM workdays WHERE user_id = ?", userID).Scan(&days)
-		err := db.QueryRowContext(ctx, "SELECT days FROM workdays WHERE user_id = $1", userID).Scan(&days)
+		err := db.QueryRow("SELECT days FROM workdays WHERE user_id = $1", userID).Scan(&days)
 		if err != nil && err != sql.ErrNoRows {
 			log.Println(err)
 			log.Fatal(err)
@@ -139,22 +133,22 @@ func DBconnection(dbsource string) *sql.DB {
 	return db
 }
 
-type Config struct {
-	Token          string `mapstructure:"TELEGRAM_TOKEN"`
-	DatabaseSource string `mapstructure:"DBSOURCE"`
-}
+// type Config struct {
+// 	token          string `mapstructure:"TELEGRAM_TOKEN"`
+// 	satabaseSource string `mapstructure:"DBSOURCE"`
+// }
 
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("config")
-	viper.SetConfigType("env")
+// func LoadConfig(path string) (config Config, err error) {
+// 	viper.AddConfigPath(path)
+// 	viper.SetConfigName("config")
+// 	viper.SetConfigType("env")
 
-	viper.AutomaticEnv()
+// 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
-	err = viper.Unmarshal(&config)
-	return
-}
+// 	err = viper.ReadInConfig()
+// 	if err != nil {
+// 		return
+// 	}
+// 	err = viper.Unmarshal(&config)
+// 	return
+// }
